8c1847b68c7cbbfb611c36f54bfd47702b5e0dce
koheishinpuku <kohei.shinpuku@unimal.jp>
2020-11-25 21:16:31 +0900

    一回目

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # このアクションを追加
+    def after_sign_in_path_for(resource)
+        "/books"
+    end
+
+    def after_sign_out_path_for(resource)
+        "/users/sign_in"
+    end
+
+
+    protected
+
+    # 入力フォームからアカウント名情報をDBに保存するために追加
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+end
